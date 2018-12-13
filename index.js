8d995fdc8aabd5601f1a3d1c280ba8fc41e8b32f
vyskoutaris <vyskoutaris@hotmail.com>
2018-12-13 21:11:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+
+//Basic Math
+
+var number;
+function add (a, b) {
+ number = a + b;
+ return number;
+ }
+ 
+function subtract (a, b) {
+ number = b - a;
+ return number;
+}
+
+function multiply (a, b) {
+ number = a * b;
+ return number;
+}
+ 
+function divide (a, b)  {
+ number = a / b;
+ return number;
+}
